a00ed1f228d9f788e9f1de49732495c19162cbb1
Melisa KMNV <melisakmnv@gmail.com>
2022-02-24 01:39:40 +0100

    import modules & set ENV  -BackEnd

--- ompale-back/server.js
+++ ompale-back/server.js
@@ -1,2 +1,15 @@
-const hello = "hello"
-console.log(hello);
+// #region IMPORT MODULES
+import express from 'express';
+import morgan from 'morgan';
+import colors from 'colors';
+import dotenv from 'dotenv';
+// #endregion IMPORT
+
+dotenv.config()
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`server is running in ${process.env.NODE_ENV} on port ${PORT}`.magenta.bold))
